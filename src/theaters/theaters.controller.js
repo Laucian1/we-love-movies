c7bfbf1fba1c22dd51e3b408ebff27e7f3ecd063
Russell Smith <resmithjr88@gmail.com>
2022-09-09 10:26:17 -0600

    Completed theater display functionality with movies mapped and parsed to pass test parameters

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -1,13 +1,29 @@
 const service = require("./theaters.service")
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
+const reduceProperties = require("../utils/reduce-properties")
 
 async function list(req, res) {
-    const data = await service.list()
-    res.json({
-        data,
+    const reduceMovies = reduceProperties("theater_id", {
+        movie_id: ["movies", null, "movie_id"],
+        title: ["movies", null, "title"],
+        runtime_in_minutes: ["movies", null, "runtime_in_minutes"],
+        rating: ["movies", null, "rating"],
+        description: ["movies", null, "description"],
+        image_url: ["movies", null, "image_url"],
+        created_at: ["movies", null, "created_at"],
+        updated_at: ["movies", null, "updated_at"],
+        is_showing: ["movies", null, "is_showing"],
+        theater_id: ["movies", null, "theater_id"]
     })
+
+    const data = await service.list()
+    const parsed = data.map(({ runtime_in_minutes, ...data}) => {
+        return { runtime_in_minutes: Number(runtime_in_minutes), ...data }
+    })
+    res.json({ data: reduceMovies(parsed) })
 }
 
+
 module.exports = {
-    list: asyncErrorBoundary(list),
+    list: asyncErrorBoundary(list)
 }
--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,7 +1,11 @@
 const knex = require("../db/connection")
 
-async function list() {
-    return knex("theaters").select("*")
+function list() {
+    return knex("theaters as t")
+        .join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
+        .join("movies as m", "mt.movie_id", "m.movie_id")
+        .select("t.*", "m.*", "mt.*")
+        .where({ is_showing: true })
 }
 
 module.exports = {
