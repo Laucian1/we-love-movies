df775970f288e983551a11262360b05f26f233e0
Russell Smith <resmithjr88@gmail.com>
2022-09-17 07:25:25 -0600

    updated package json

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors")
 
 const errorHandler = require("./errors/errorHandler")
 const notFound = require("./errors/notFound")
@@ -10,21 +11,10 @@ const theatersRouter = require("./theaters/theaters.router")
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
-    if (req.method === "OPTIONS") {
-      res.header(
-        "Access-Control-Allow-Methods",
-        "POST, PUT, PATCH, GET, DELETE"
-      )
-      return res.status(200).json({})
-    }
-    next()
-  })
+app.use(cors({
+    origin: '*'
+}));
+
 app.use(express.json())
 app.get("/", (req, res) => res.status(301).redirect("/movies"))
 
