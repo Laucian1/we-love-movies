982ded9f5a72b9ba9f84e078fda334bc4eae5cef
Russell Smith <resmithjr88@gmail.com>
2022-09-16 18:10:18 -0600

    Added explicit cors access

--- src/app.js
+++ src/app.js
@@ -14,6 +14,23 @@ const app = express();
 app.use(cors({
     origin: '*'
 }));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "PUT, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  });
+
 app.use(express.json())
 app.get("/", (req, res) => res.status(301).redirect("/movies"))
 
